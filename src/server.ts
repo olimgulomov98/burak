629ec73f12410faa0e7c736ce85817919d5b912a
Olim <olimgulomov@gmail.com>
2024-02-26 17:48:43 +0400

    feat: integration of environmental variables

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,2 @@
-// console.log("EXECUTED");
-
-// import moment from "moment";
-
-// const currentTime = moment().format("YYYY MM DD");
-// console.log(currentTime);
-
-// const person: string = "Martin";
-// const count: number = 100;
-
-// Architectural pattern: MVC, Dependency Injection, MVP
-
-// MVC - module view controller
-
-// Design pattern: Middleware, Decotar
+import dotenv from "dotenv";
+dotenv.config();
--- train.js
+++
@@ -1,6 +0,0 @@
-function getHighestIndex(arr) {
-  let highestIndex = arr.indexOf(Math.max(...arr));
-  return highestIndex;
-}
-
-console.log(getHighestIndex([5, 21, 12, 21, 8]));
