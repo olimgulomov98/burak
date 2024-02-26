c4d2393e2ab7cce3c182916990b87ab2f4f76d20
Olim <olimgulomov@gmail.com>
2024-02-26 21:14:15 +0400

    feat: H-TASK

--- src/train.ts
+++ src/train.ts
@@ -1,1 +1,14 @@
-console.log("HELLO WORLD");
+// H-TASK
+
+function getReverse(str: string) {
+  // 1 method - by reverse
+  //   return str.split("").reverse().join("");
+
+  // 2 method - by for loop
+  let result = "";
+  for (let i = str.length - 1; i >= 0; i--) {
+    result += str[i];
+  }
+  return result;
+}
+console.log(getReverse("hello"));
