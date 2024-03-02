b8fc9915e6f0b68e2d24465cd556835234077b7e
Olim <olimgulomov@gmail.com>
2024-03-02 08:21:09 +0400

    feat: create project standards

--- src/app.ts
+++ src/app.ts
@@ -2,18 +2,21 @@ import express from "express"; // backend frameworks uchun
 import path from "path";
 import router from "./router";
 import routerAdmin from "./routerAdmin";
+import morgan from "morgan";
+import { MORGAN_FORMAT } from "./libs/config";
 
 /** 1-ENTRANCE **/
 const app = express();
-app.use(express.static(path.join(__dirname, "public")));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, "public"))); // static - folder ni ochiq qilib beradi
+app.use(express.urlencoded({ extended: true })); // urlencoded - form dan kelayotgan request larni parse qilib object ga uzgartiradi
 app.use(express.json());
+app.use(morgan(MORGAN_FORMAT));
 
 /** 2-SESSIONS **/
 
 /** 3-VIEWS **/
-app.set("views", path.join(__dirname, "views"));
-app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views")); // config qilib beradi
+app.set("view engine", "ejs"); // config qilib beradi
 
 /** 4-ROUTERS **/
 app.use("/admin", routerAdmin); // EJS
