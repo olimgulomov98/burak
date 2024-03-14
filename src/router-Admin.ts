b2d861e18d8807a82ee0bf378cd6caa8acaf14d6
Olim <olimgulomov@gmail.com>
2024-03-14 10:52:13 +0400

    feat: locals variables integration

--- src/router-Admin.ts
+++ src/router-Admin.ts
@@ -15,9 +15,21 @@ routerAdmin.get("/logout", restaurantController.logout);
 routerAdmin.get("/check-me", restaurantController.checkAuthSession);
 
 /** Product */
-routerAdmin.get("/product/all", productController.getAllProduct);
-routerAdmin.post("/product/create", productController.createNewProduct);
-routerAdmin.post("/product/:id", productController.updateChosenProduct);
+routerAdmin.get(
+  "/product/all",
+  restaurantController.verifyRestaurant,
+  productController.getAllProduct
+);
+routerAdmin.post(
+  "/product/create",
+  restaurantController.verifyRestaurant,
+  productController.createNewProduct
+);
+routerAdmin.post(
+  "/product/:id",
+  restaurantController.verifyRestaurant,
+  productController.updateChosenProduct
+);
 
 /** User */
 
