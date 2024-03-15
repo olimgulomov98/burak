8f9f9e7cba449b6974fc692b0e0033f51608b15a
Olim <olimgulomov@gmail.com>
2024-03-15 23:57:01 +0400

    fix: modify restaurant controller processSignup and processLogin

--- src/train.ts
+++ src/train.ts
@@ -8,3 +8,30 @@ function reverseSentence(str: string): string {
 }
 
 console.log(reverseSentence("we like coding!"));
+
+/* Project Standards:
+  - Logging standards
+  - Naming standards
+    CAMEL case : function, method, variable 
+    PASCAL : class                            
+    KEBAB case : folder - file
+    SNAKE case : css
+  - Error handling
+*/
+
+/* Request:
+Traditional Api
+Rest Api
+GraphQL Api
+*/
+
+/* Cookie:
+  request: join
+  self: destroy
+*/
+
+/* Validation:
+  Frontend: validation
+  Backend: validation
+  Database: validation
+*/
