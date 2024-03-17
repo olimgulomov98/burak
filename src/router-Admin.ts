6ac7f9530ddd0da826db2933650394a973f44bde
Olim <olimgulomov@gmail.com>
2024-03-18 01:24:48 +0400

    feat: develop getUsers business logic

--- src/router-Admin.ts
+++ src/router-Admin.ts
@@ -3,6 +3,7 @@ const routerAdmin = express.Router();
 import restaurantController from "./controllers/restaurant.controller";
 import productController from "./controllers/product.controller";
 import makeUploader from "./libs/utils/uploader";
+
 /** Restaurant */
 routerAdmin.get("/", restaurantController.goHome);
 routerAdmin
@@ -38,5 +39,10 @@ routerAdmin.post(
 );
 
 /** User */
+routerAdmin.get(
+  "/user/all",
+  restaurantController.verifyRestaurant,
+  restaurantController.getUsers
+);
 
 export default routerAdmin;
